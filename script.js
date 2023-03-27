4b5ebeee958e3b3a59e5b07f301b23005449057b
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-03-27 14:20:17 +0100

    money in / out and interest calc and disp in html

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ const displayMovements = function (movements) {
     const html = `
       <div class="movements__row">
       <div class="movements__type movements__type--${type}">${i + 1}. ${type}</div>
-      <div class="movements__value">${mov}</div>
+      <div class="movements__value">${mov}€</div>
       </div>
     `
 
@@ -87,11 +87,24 @@ const calcDisplayBalance = function (movements) {
 
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
 
-  labelBalance.textContent = `GBP ${balance}`;
+  labelBalance.textContent = `€${balance}`;
 
 };
 
 calcDisplayBalance(account1.movements);
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements.filter(mov => mov > 0).reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `€${incomes}`;
+
+  const out = movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `€${Math.abs(out)}`;
+
+  const interest = movements.filter(mov => mov > 0).map(deposit => deposit * (1.2 / 100)).filter((int, i, arr) => int >= 1).reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `€${interest}`;
+};
+
+calcDisplaySummary(account1.movements);
 
 
 const createUsernames = function (accs) {
@@ -122,6 +135,8 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 const eurToUsd = 1.1;
 
+const totalDepositsUSD = movements.filter(mov => mov > 0).map(mov => mov * eurToUsd).reduce((acc, mov) => acc + mov, 0);
+
 // const movementsUSD = movements.map(function (mov) {
 //   return mov * eurToUsd;
 // });
@@ -144,4 +159,5 @@ const withdrawals = movements.filter(mov => mov < 0);
 // accumulator -> snowball
 const balance = movements.reduce((acc, cur) => acc + cur, 0);
 
-console.log(balance);
+
+const max = movements.reduce((acc, mov) => acc > mov ? acc : mov, movements[0]);
